e53887734e29489315b19af46ecd22ecfcce62a9
misung.dev <misung.dev@gmail.com>
2025-01-12 03:35:24 +0900

    feat: 로그인 타이틀에 컴포넌트 적용

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,39 +1,46 @@
-import { BottomBar, PageLayout } from "../../components";
+import { BottomBar, NewsHeader, PageLayout } from "../../components";
 import * as S from "./LoginPage.style";
 import { useState } from "react";
 
 const LoginPage = () => {
-
   const [disabled, setDisabled] = useState(true);
 
-  const [id, setId] = useState('');
-  const [passwd, setPasswd] = useState('');
-
+  const [id, setId] = useState("");
+  const [passwd, setPasswd] = useState("");
 
   return (
     <PageLayout footer={<BottomBar />}>
       <S.LoginPage>
-      <S.LoginTitleWrapper>
-        <S.LoginTitle>
-          로그인 후에 나만의 뉴스를 작성해주세요.
-        </S.LoginTitle> 
-        <S.Ellipse src="/svgs/ellipse.svg"/>
-      </S.LoginTitleWrapper>
-      <S.Gap $gap={'25px'}/>
-      <S.InputDiv>
-        <S.Input maxLength='10' placeholder="아이디를 입력하세요." value={id} onChange={(e) => setId(e.target.value)}/>
-        <S.TextNum>{id.length}/10</S.TextNum>
-      </S.InputDiv>
-      <S.Gap $gap={'8px'}/>
-      <S.InputDiv>
-        <S.Input maxLength='20' placeholder="비밀번호를 입력하세요." value={passwd} onChange={(e) => setPasswd(e.target.value)}/>
-        <S.TextNum>{passwd.length}/20</S.TextNum>
-      </S.InputDiv>
-      <S.Gap $gap={'16px'}/>
-      <S.LoginButton disabled={disabled}>로그인</S.LoginButton>
+        <S.LoginTitleWrapper>
+          <NewsHeader title="로그인 후에 나만의 뉴스를 작성해주세요." />
+        </S.LoginTitleWrapper>
+        <S.Gap $gap={"25px"} />
+        <S.InputWrapper>
+          <S.InputDiv>
+            <S.Input
+              maxLength="10"
+              placeholder="아이디를 입력하세요."
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+            />
+            <S.TextNum>{id.length}/10</S.TextNum>
+          </S.InputDiv>
+          <S.Gap $gap={"8px"} />
+          <S.InputDiv>
+            <S.Input
+              maxLength="20"
+              placeholder="비밀번호를 입력하세요."
+              value={passwd}
+              onChange={(e) => setPasswd(e.target.value)}
+            />
+            <S.TextNum>{passwd.length}/20</S.TextNum>
+          </S.InputDiv>
+          <S.Gap $gap={"16px"} />
+          <S.LoginButton disabled={disabled}>로그인</S.LoginButton>
+        </S.InputWrapper>
       </S.LoginPage>
     </PageLayout>
-  )
+  );
 };
 
 export default LoginPage;
--- src/pages/LoginPage/LoginPage.style.jsx
+++ src/pages/LoginPage/LoginPage.style.jsx
@@ -3,29 +3,23 @@ import styled from "styled-components";
 export const LoginPage = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 0 16px;
-`
+`;
 
 export const LoginTitleWrapper = styled.div`
   width: 100%;
   height: 43px;
   margin-top: 120px;
-  padding: 8px 0;
-  color: var(--gray-500)
+  color: var(--gray-500);
   font-size: 18px;
   font-weight: 700;
   display: flex;
-`
-
-export const LoginTitle = styled.p`
-  align-content: center;
-`
+`;
 
 export const Ellipse = styled.img`
   width: 8px;
   height: 8px;
   margin-left: 8px;
-`
+`;
 
 export const InputDiv = styled.div`
   display: flex;
@@ -33,11 +27,16 @@ export const InputDiv = styled.div`
   border-radius: 10px;
   border: 1px solid var(--gray-300);
   align-content: center;
-`
+  padding: 0 16px;
+`;
 
 export const Gap = styled.div`
-  height: ${props => props.$gap}
-`
+  height: ${(props) => props.$gap};
+`;
+
+export const InputWrapper = styled.div`
+  padding: 0 16px;
+`;
 
 export const Input = styled.input`
   outline: 0;
@@ -51,15 +50,16 @@ export const Input = styled.input`
   &::placeholder {
     color: var(--gray-400);
   }
-`
+`;
 
 export const TextNum = styled.p`
-  color: ${props => props.$color || `var(--gray-400)`};
+  color: ${(props) => props.$color || `var(--gray-400)`};
   font-size: var(--font-caption-13);
   padding: 14px 18px;
-`
+`;
 
 export const LoginButton = styled.button`
+  width: 100%;
   height: 40px;
   background-color: var(--gray-500);
   border-radius: 8px;
@@ -68,8 +68,8 @@ export const LoginButton = styled.button`
   font-size: var(--font-title-16);
   font-weight: 700;
 
-  &:disabled{
+  &:disabled {
     background-color: var(--gray-300);
     color: var(--gray-400);
   }
-`
+`;
