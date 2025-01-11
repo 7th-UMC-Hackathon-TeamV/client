356de480fa82ee81fee2d940313be358a407e6af
misung.dev <misung.dev@gmail.com>
2025-01-11 22:07:18 +0900

    modify: 하단 네비게이션 너비 수정

--- src/components/BottomBar/BottomBar.style.jsx
+++ src/components/BottomBar/BottomBar.style.jsx
@@ -1,29 +1,25 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const BottomBar = styled.div`
-  width: 100vw;
-  height: 82px;
-
   display: flex;
-  justify-content: space-evenly;
-`
+  justify-content: space-around;
+  width: 100%;
+`;
 
 export const ContentWrapper = styled.button`
-  width: 58px;
-  height: 86px;
-
   display: flex;
   flex-direction: column;
-
-  justify-content: space-evenly;
   align-items: center;
-`
+  width: 100%;
+  padding: 10px;
+  gap: 4px;
+`;
 
 export const ContentIcon = styled.img`
   width: 24px;
   height: 24px;
-`
+`;
 
 export const ContentText = styled.p`
   font-size: 13px;
-`
+`;
