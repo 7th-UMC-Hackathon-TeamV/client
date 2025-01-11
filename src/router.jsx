b94b6075b9da76efef7e5ea1f21a6b7e0f8f848a
이준희 <0210leejun@gmail.com>
2025-01-12 02:48:31 +0900

    feat: create-mypage

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { HomePage, LoginPage, NewsPage, UpdateNewsPage } from "./pages";
+import { HomePage, LoginPage, NewsPage, UpdateNewsPage, MyPage } from "./pages";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: "/update-news",
         element: <UpdateNewsPage />,
+      },
+      {
+        path: "/mypage",
+        element: <MyPage />,
       },
     ],
   },
