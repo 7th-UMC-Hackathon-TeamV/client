0bc0c05d162970a8658143536cefdfc78ee33748
pkyujin <luxoris02@sookmyung.ac.kr>
2025-01-12 03:08:08 +0900

    feat: 속보 스와이프 넘기기 구현

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   BottomBar,
   NewsAlertBox,
@@ -7,9 +8,26 @@ import {
   NewsCardList,
 } from "../../components";
 import * as S from "./HomePage.style";
+import Slider from "react-slick"; // react-slick import
 
 const HomePage = () => {
   const USER_ID_LIST = ["v0111", "v0132", "올라"]; // 추후 삭제하고 API 연동 통해서 userID 가져와야함
+
+  const hotNewsData = [
+    { title: "News 1", type: "Type A", backgroundImage: "image1.jpg" },
+    { title: "News 2", type: "Type B", backgroundImage: "image2.jpg" },
+    { title: "News 3", type: "Type C", backgroundImage: "image3.jpg" },
+    // 추가 데이터
+  ];
+
+  // Slider 설정
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
     <PageLayout footer={<BottomBar />}>
@@ -26,7 +44,16 @@ const HomePage = () => {
         <S.NewsSectionWrapper>
           <S.NewsSection>
             <NewsHeader title="어제의 핫! 온에어" />
-            <HotNewsCard />
+            <Slider {...sliderSettings}>
+              {hotNewsData.map((news, index) => (
+                <HotNewsCard
+                  key={index}
+                  title={news.title}
+                  type={news.type}
+                  backgroundImage={news.backgroundImage}
+                />
+              ))}
+            </Slider>
           </S.NewsSection>
 
           <S.NewsSection>
