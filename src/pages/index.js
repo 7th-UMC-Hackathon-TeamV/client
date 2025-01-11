89d6beff73f02b148d94b8f47b8ad3de88368fe5
이준희 <0210leejun@gmail.com>
2025-01-12 02:48:31 +0900

    feat: create-mypage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { HomePage } from "./HomePage";
 export { LoginPage } from "./LoginPage";
 export { NewsPage } from "./NewsPage";
 export { UpdateNewsPage } from "./UpdateNewsPage";
+export { MyPage } from "./MyPage";
