835d215f569df66527dcca66d3f6669c508ffcd1
pkyujin <luxoris02@sookmyung.ac.kr>
2025-01-12 01:20:14 +0900

    feat: 6시 전 홈화면 제작

--- src/components/NewsCard/NewsCard.style.js
+++ src/components/NewsCard/NewsCard.style.js
@@ -9,41 +9,24 @@ export const PageContainer = styled.div`
   justify-content: center;
 `;
 
-export const HotCardContainer = styled.div`
+export const CardListWrapper = styled.div`
+  display: flex;
+  flex-direction: column; 
+  width: 100%;
+`;
+
+export const CardContainer = styled.div`
   width: 85%;
-  height: 100%;
+  height: 75%;
   background: #dfdfdf;
   border-radius: 8px;
   display: flex;
   justify-content: flex-start;
   position: relative;
+  margin-top: -30px;
 `;
 
-export const HotIcon = styled.div`
-  display: flex;
-  justify-content: left;
-  margin-top: 6px;
-  padding-left: 14px;
-  width: 80px;
-  height: 35px;
-  border-radius: 20px;
-  background: #222222;
-  position: absolute;
-  top: 10px;
-  left: 20px;
-`;
-
-export const HOTLabel = styled.p`
-  color: white;
-  font-size: 22px;
-  font-weight: 1000;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  justify-content: center;
-`;
-
-export const HotTitleWrapper = styled.div`
+export const TitleWrapper = styled.div`
   display: flex;
   width: 70%;
   height: 18%;
@@ -52,29 +35,29 @@ export const HotTitleWrapper = styled.div`
   bottom: 20px;
 `;
 
-export const HotTitleBox = styled.div`
+export const TitleBox = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
   overflow: hidden;
 `
 
-export const HotTitle = styled.p`
+export const Title = styled.p`
   color: white;
-  font-size: 27px;
+  font-size: 24px;
   font-weight: 800;
   justify-content: center;
   align-items: center;
-  margin-top: 10px;
+  margin-top: 6px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis; 
 `;
 
 
-export const HotType = styled.div`
+export const Type = styled.div`
   background: ${({ type }) => type === 'True' ? '#E22c2f' : '#18309F'};
-  width: 20px;
+  width: 18px;
   height: 100%;
   margin-right: 15px;
 `;
