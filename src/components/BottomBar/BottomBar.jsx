061388fc162ea4575b263cae0c3d4be27b4f321c
이준희 <0210leejun@gmail.com>
2025-01-12 05:03:42 +0900

    feat: bottombar-routing

--- src/components/BottomBar/BottomBar.jsx
+++ src/components/BottomBar/BottomBar.jsx
@@ -1,22 +1,26 @@
 import * as S from "./BottomBar.style";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const BottomBar = () => {
   const navigator = useNavigate();
 
+  const { groupKey } = useParams();
+
+  console.log(groupKey)
+
   return (
     <S.BottomBar>
-      <S.ContentWrapper onClick={() => navigator("/home")}>
+      <S.ContentWrapper onClick={() => navigator(`/${groupKey}`)}>
         <S.ContentIcon src="/svgs/home.svg" />
         <S.ContentText>홈</S.ContentText>
       </S.ContentWrapper>
 
-      <S.ContentWrapper onClick={() => navigator("/update-news")}>
+      <S.ContentWrapper onClick={() => navigator(`/update-news/${groupKey}`)}>
         <S.ContentIcon src="/svgs/pencil.svg" />
         <S.ContentText>작성</S.ContentText>
       </S.ContentWrapper>
 
-      <S.ContentWrapper onClick={() => navigator("/login")}>
+      <S.ContentWrapper onClick={() => navigator(`/login/${groupKey}`)}>
         <S.ContentIcon src="/svgs/user.svg" />
         <S.ContentText>마이페이지</S.ContentText>
       </S.ContentWrapper>
