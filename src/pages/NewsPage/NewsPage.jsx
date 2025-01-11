7f187e85aea2f6ea895db901269f7175610c3b20
이준희 <0210leejun@gmail.com>
2025-01-12 01:23:39 +0900

    feat: 뉴스 상세 페이지 구현

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
 import { BottomBar, PageLayout } from "../../components";
 import * as S from "./NewsPage.style";
 
 const NewsPage = () => {
+
+  const titleColor = 'red';
+  
+  const [likeCount, setLikeCount] = useState(0);
+  const [isLike, setIsLike] = useState(false);
+
+  const handleLikeButton = () => {
+    if (isLike){
+      setLikeCount(likeCount-1)
+    }
+    else {
+      setLikeCount(likeCount+1)
+    }
+    setIsLike(!isLike);
+  }
+
   return (
     <PageLayout footer={<BottomBar />}>
-      <S.NewsPage>뉴스페이퍼</S.NewsPage>
+      <S.NewsPage>
+        <S.Back src="/svgs/back.svg" onClick={() => { }} />
+        <S.NewsContents>
+          <S.Badge>속보</S.Badge>
+          <S.NewsTitle $color={titleColor}>bad은 빨강, good은 파랑으로 됩니다.</S.NewsTitle>
+          <S.NewsImage src="/svgs/likeon.svg"/>
+          <S.NewsDetail>
+            본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문
+          </S.NewsDetail>
+          <S.LikeWrapper>
+            <S.LikeButton onClick={() => handleLikeButton()}>
+              <S.LikeIcon src={isLike? "/svgs/likeon.svg" : "/svgs/likeoff.svg"}/>
+            </S.LikeButton>
+            <S.LikeCount>{likeCount}</S.LikeCount>
+          </S.LikeWrapper>
+        </S.NewsContents>
+      </S.NewsPage>
     </PageLayout>
   );
 };
--- src/pages/NewsPage/NewsPage.style.jsx
+++ src/pages/NewsPage/NewsPage.style.jsx
@@ -3,4 +3,74 @@ import styled from "styled-components";
 export const NewsPage = styled.div`
   display: flex;
   flex-direction: column;
+
+  padding: 0 16px;
 `;
+
+export const Back = styled.img`
+  position: absolute;
+  top: 24px;
+  left: 16px;
+`
+
+export const NewsContents = styled.div`
+  width: 100%; 
+  margin-top: 80px;
+`
+
+export const Badge = styled.div`
+  width: 59px;
+  height: 34px;
+
+  border-radius: 8px;
+  background-color: var(--gray-500);
+  color: var(--gray-100);
+
+  font-size: var(--font-caption-13)
+  font-weight: 700;
+
+  text-align: center;
+  align-content: center;
+`
+
+export const NewsTitle = styled.p`
+  font-size: var(--font-title-16);
+  font-weight: 700;
+  height: 24px;
+
+  margin-top: 16px;
+  color: ${props => props.$color};
+`
+
+export const NewsImage = styled.img`
+  height: 160px;
+  margin-top: 16px;
+`
+
+export const NewsDetail = styled.div`
+  margin-top: 16px;
+  padding: 8px;
+  font-size: var(--font-body-14);
+  color: var(--gray-500);
+`
+
+export const LikeWrapper = styled.div`
+  margin-top: 16px;
+  align-items: center;
+  display: flex;
+  height: 24px;
+  gap: 8px;
+`
+
+export const LikeButton = styled.div`
+`
+
+export const LikeIcon = styled.img` 
+  width: 24px;
+  height: 24px;
+`
+
+export const LikeCount = styled.div`
+  font-size: var(--font-caption-13);
+  font-weight: 700;
+`
